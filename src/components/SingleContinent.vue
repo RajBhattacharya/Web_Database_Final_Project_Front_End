3ea35477a90be0d788171a009e1dc5a2325b7853
RajBhattacharya <49310816+RajBhattacharya@users.noreply.github.com>
2023-12-13 12:17:24 -0500

    Update SingleContinent.vue

--- src/components/SingleContinent.vue
+++ src/components/SingleContinent.vue
@@ -1,11 +1,13 @@
 <template>
-    <div :class="continent.country_name=='United States of America'? 'green': 'red'">
-    <h2>{{continent.continent_name}}</h2>
-                        <h4> {{continent.continent_id}}</h4>
-                        <h4> {{continent.continent_name}}</h4>
-                        <h4> {{continent.country_id}}</h4>
-                        <h4> {{continent.country_name}}</h4>
-                        <h4> {{continent.capital_name}}</h4>
+    <div :class="continent.country_name=='United States of America'? 'deepblue': 'blue'">
+                        <h2> Continents, Countries & Capitals: {{continent.continent_name}}</h2>
+                        <div2 :class="continent.country_name=='United States of America'? 'deepblue': 'blue'">
+                        
+                        <h4> Continent ID: {{continent.continent_id}}</h4>
+                        <h4> Continent Name: {{continent.continent_name}}</h4>
+                        <h4> Country ID: {{continent.country_id}}</h4>
+                        <h4> Country Name: {{continent.country_name}}</h4>
+                        <h4> Capital Name: {{continent.capital_name}}</h4></div2>
     </div>
 </template>
 
@@ -20,24 +22,47 @@ export default{
 
 <style scoped>
 
-.green{
-    background-color: rgb(78, 240, 78);
+.deepblue{
+    background-color: rgb(21, 54, 238);
+    border: 1px solid rgb(2, 40, 255);
+    color: white;
 }
 
-h4{
+h4{  
     margin: 2px;
+    text-align: left;
 }
 
 div{
-    background-color: #ff672f;
+    width: 80%;
+    height: 300px;
+    border: 1px solid rgb(255, 255, 255);
+    background-color: rgb(255, 255, 255);
     color: black;
+    font-family: 'Times New Roman', Times, serif;
     padding: 1em;
     text-align: center;
-    width: 80%;
     margin: auto;
+    position: relative;
 }
 
-.red{
-background-color: rgb(255, 204, 185) !important;
+div2{
+      width: 90%;
+      height: 60%;
+      background-color: rgb(203, 211, 255);
+      color: rgb(51, 51, 56);
+      font-family: 'Times New Roman', Times, serif;
+      font-size: 18px;
+      letter-spacing: 2px;
+      text-align: center;
+      line-height: 250%;
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+    }
+
+.blue{
+background-color: rgb(255, 255, 255) !important;
 }
 </style>
